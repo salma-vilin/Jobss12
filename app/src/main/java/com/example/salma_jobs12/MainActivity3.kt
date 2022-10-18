a91cdb4809e5afa30551efdea2f0f624055e74f5
anita <anita@gmail.com>
2022-10-18 12:03:10 +0700

    first commit

---
+++ app/src/main/java/com/example/salma_jobs12/MainActivity3.kt
@@ -0,0 +1,11 @@
+package com.example.salma_jobs12
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity3 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main3)
+    }
+}
